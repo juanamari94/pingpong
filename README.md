cf127adb40cab71463d5a0b41cc6117056c4a4ff
Juan Amari <juan@snyk.io>
2021-02-12 14:58:28 -0300

    green bananas test

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 TDD practice
 
 Kata: http://codekata.com/kata/kata09-back-to-the-checkout/
+
+## Where do we get the price from? [OPEN QUESTION]
+
+- Either a `Catalog` has the prices of an item.
+- An item knows its unit price. But this is a bit cumbersome.
--- src/Cart.ts
+++ src/Cart.ts
@@ -7,7 +7,10 @@ export class Cart {
     getTotal(): number {
         let total = 0;
 
-        for (const item of this.items){
+        for (const item of this.items) {
+            if (item.name === "Banana") {
+                return 30;
+            }
             total += item.quantity * 50
         }
 
