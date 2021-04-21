0e79ae4f52094ddb4838e254257ada74a41b0b3f
Juan Amari <juan@snyk.io>
2021-04-21 15:50:30 +0300

    clean slate

--- src/Item.ts
+++
@@ -1,9 +0,0 @@
-export class Item {
-    name: string;
-    quantity: number;
-
-    constructor(name: string, quantity: number) {
-        this.name = name;
-        this.quantity = quantity;
-    }
-}
